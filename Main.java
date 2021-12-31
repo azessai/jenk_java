5669e34068968a6da0db4ec5f047f930e473a8da
azessai <azeddine.essai@gmail.com>
2021-12-31 22:19:37 +0000

    change hello

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
         public static void main(String[] args) {
-                System.out.println("Hello, Azeddine!");
+                System.out.println("Hello, Azeddine from master!");
         }
 }
